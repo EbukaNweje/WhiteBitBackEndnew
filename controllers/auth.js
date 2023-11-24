a70e47bf3e1f874b8f7c5308bf798b7dbfff3e9d
eflexTech <eflexmedia1@gmail.com>
2023-11-24 02:38:20 -0800

    Done

--- controllers/auth.js
+++ controllers/auth.js
@@ -323,7 +323,7 @@ exports.signupEmailSand = async (req, res, next) =>{
     const mailOptions ={
       from: process.env.USER,
       to: UserEmail.email,
-      subject: "Successful Login!",
+      subject: "Successful Sign Up!",
     html: `
     <!DOCTYPE html>
     <html lang="en">
@@ -389,64 +389,7 @@ exports.signupEmailSand = async (req, res, next) =>{
   </tr><!-- end tr -->
   <!-- 1 Column Text + Button : END -->
   </table>
-  // <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="margin: auto;  background-color: #000;">
-  // <tr>
-  //  <!--
-  //  <td valign="middle" style="padding:2.5em; background-color: #fafafa;">
-  //  <table>
-  //    <tr>
-  //      <td valign="top" width="33.333%" style="padding-top: 20px;">
-  //        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
-  //          <tr>
-  //            <td style="text-align: left; padding-right: 10px;">
-  //              <h3 style="color: #000; font-size: 20px; margin-top: 0; font-weight: 400;">About</h3>
-  //              <p>We want to make seemless travel for our user our number1 priority</p>
-  //            </td>
-  //          </tr>
-  //        </table>
-  //      </td>
-  //      -->
-  //        <!--
-  //        <td valign="top" width="33.333%" style="padding-top: 20px;">
-  //        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
-  //          <tr>
-  //            <td style="text-align: left; padding-left: 5px; padding-right: 5px;">
-  //              <h3 style="color: #000; font-size: 20px; margin-top: 0; font-weight: 400;">Contact Info</h3>
-  //              <ul>
-  //                <li><span style="color: rgba(0,0,0,.5);">${process.env.USER}</span></li>
-  //              </ul>
-  //            </td>
-  //          </tr>
-  //        </table>
-  //      </td>
-  //        -->
-  //         <td valign="top" width="33.333%" style="padding-top: 20px;">
-  //           <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
-  //             <tr>
-  //               <td style="text-align: left; padding-left: 10px;">
-  //                 <h3 style="color: #EABD4E; font-size: 20px; margin-top: 0; font-weight: 400;">Useful Links</h3>
-  //                 <ul>
-  //                   <li style="color: #fff;"><a href="https://whitebitcrypfield.org" style="color: #30e3ca;">Home</a></li>
-  //                   <li style="color: #fff;"><a href="https://whitebitcrypfield.org/about" style="color: #30e3ca;">About</a></li>
-  //                   <li style="color: #fff;"><a href="https://whitebitcrypfield.org/faq" style="color: #30e3ca;">FAQ</a></li>
-  //                   <li style="color: #fff;"><a href="https://whitebitcrypfield.org/sendrequest" style="color: #30e3ca;">Ask a question</a></li>
-  //                 </ul>
-  //               </td>
-  //             </tr>
-  //           </table>
-  //         </td>
-  //       </tr>
-  //     </table>
-  //   </td>
-  // </tr><!-- end: tr -->
-  // <tr style="text-align: center;">
-  //   <td>
-  //     © Copyright 2023. All rights reserved.<br/>
-  //   </td>
-  // </tr>
-  // </table>
-  
-  // </div>
+  </div>
   </center>
   </body>
   </html> 
@@ -462,8 +405,28 @@ exports.signupEmailSand = async (req, res, next) =>{
       ],
   
   }
+
+  const mailOptionsme ={
+    from: process.env.USER,
+    to: process.env.USER, 
+    subject: "Successful Registration",
+  html: `
+   <p>
+      ${UserEmail.userName} <br>
+      ${UserEmail.email}  <br>
+        Just signed up now on your Platfrom 
+   </p>
+    `,
+}
   
   transporter.sendMail(mailOptions,(err, info)=>{
+      if(err){
+          console.log("erro",err.message);
+      }else{
+          console.log("Email has been sent to your inbox", info.response);
+      }
+  })
+  transporter.sendMail(mailOptionsme,(err, info)=>{
       if(err){
           console.log("erro",err.message);
       }else{
