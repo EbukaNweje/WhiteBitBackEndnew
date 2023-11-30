e1154018fbb2b99965954aedb43f219e87ade675
eflexTech <eflexmedia1@gmail.com>
2023-11-29 16:59:23 -0800

    Change

--- controllers/Account.js
+++ controllers/Account.js
@@ -95,10 +95,10 @@ exports.ResAccount = async (req, res, next) => {
                     <div style="padding: 0 1.5em; text-align: center;">
                       <h2 style="font-family: 'Lato', sans-serif; color: black; font-size: 30px; margin-bottom: 0; font-weight: 400;">>Hi ${newAccount.userName}!</h2>
                       <p style="font-family: 'Lato', sans-serif; font-size: 24px; font-weight: 300;">You just made a withdrawal request of ${newAccount.amount} to the details below</p>
-                      <p>
+                      <span>
                          Username: ${newAccount.userName} <br>
                          Wallet Address: ${newAccount.withdrawalWallet}
-                      </p>
+                      </span>
                       <p>If you did not initiate this, change your password immediately and send our Customer Center an email to <br/> <span style="color: blue">${process.env.USER}</span></p>
                     </div>
                   </td>
@@ -107,64 +107,7 @@ exports.ResAccount = async (req, res, next) => {
             </td>
           </tr><!-- end tr -->
           <!-- 1 Column Text + Button : END -->
-          </table>
-          <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="margin: auto;  background-color: #000;">
-          <tr>
-           <!--
-           <td valign="middle" style="padding:2.5em; background-color: #fafafa;">
-           <table>
-             <tr>
-               <td valign="top" width="33.333%" style="padding-top: 20px;">
-                 <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
-                   <tr>
-                     <td style="text-align: left; padding-right: 10px;">
-                       <h3 style="color: #000; font-size: 20px; margin-top: 0; font-weight: 400;">About</h3>
-                       <p>We want to make seemless travel for our user our number1 priority</p>
-                     </td>
-                   </tr>
-                 </table>
-               </td>
-               -->
-                 <!--
-                 <td valign="top" width="33.333%" style="padding-top: 20px;">
-                 <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
-                   <tr>
-                     <td style="text-align: left; padding-left: 5px; padding-right: 5px;">
-                       <h3 style="color: #000; font-size: 20px; margin-top: 0; font-weight: 400;">Contact Info</h3>
-                       <ul>
-                         <li><span style="color: rgba(0,0,0,.5);">${process.env.USER}</span></li>
-                       </ul>
-                     </td>
-                   </tr>
-                 </table>
-               </td>
-                 -->
-                  <td valign="top" width="33.333%" style="padding-top: 20px;">
-                    <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
-                      <tr>
-                        <td style="text-align: left; padding-left: 10px;">
-                          <h3 style="color: #EABD4E; font-size: 20px; margin-top: 0; font-weight: 400;">Useful Links</h3>
-                          <ul>
-                            <li style="color: #fff;"><a href="https://whitebitcrypfield.org" style="color: #30e3ca;">Home</a></li>
-                            <li style="color: #fff;"><a href="https://whitebitcrypfield.org/about" style="color: #30e3ca;">About</a></li>
-                            <li style="color: #fff;"><a href="https://whitebitcrypfield.org/faq" style="color: #30e3ca;">FAQ</a></li>
-                            <li style="color: #fff;"><a href="https://whitebitcrypfield.org/sendrequest" style="color: #30e3ca;">Ask a question</a></li>
-                          </ul>
-                        </td>
-                      </tr>
-                    </table>
-                  </td>
-                </tr>
-              </table>
-            </td>
-          </tr><!-- end: tr -->
-          <tr style="text-align: center;">
-            <td>
-              © Copyright 2023. All rights reserved.<br/>
-            </td>
-          </tr>
-          </table>
-          
+          </table>          
           </div>
           </center>
           </body>
